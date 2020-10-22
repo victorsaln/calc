b77c176d3543e87e985285f4cede5416708a6e90
victorsaln <viktorsalnichenko@gmail.com>
2020-10-22 20:06:59 +0300

    initial

--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Форма</title>
+    <title>Кухтулятор</title>
 </head>
 <body>
-<form action="/" method="GET">
-    <label for="firstname">Имя
-        <input type="text" name="firstname" placeholder="Введите имя">
-    </label><br>
-    <input type="text" name="secondname" placeholder="Введите фамилию"><br>
-    <input type="text" name="city" placeholder="Укажите город"><br>
-    <button type="submit">Отправить</button>
+<form action="" method="POST" class="calculate-form">
+        <input type="text" name="number1" class="numbers">
+    <select class="operations" name="operation">
+        <option value='plus'>+</option>
+        <option value='minus'>-</option>
+    </select>
+        <input type="text" name="number2" class="numbers">
+    <input class="submit_form" type="submit" name="submit" value="Результат">
 </form>
 </body>
 </html>
+
+<?php
+if(isset($_POST['submit'])) {
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+    $operation = $_POST['operation'];
+    switch ($operation) {
+        case 'plus':
+            $result = $number1 + $number2;
+            break;
+        case 'minus':
+            $result = $number1 - $number2;
+            break;
+    }
+{
+        echo "<div class='answer-text'>Ответ:$number1$operation$number2 = $result</div>";
+    }
+}
+?>
