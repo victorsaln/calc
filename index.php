c7fdd5e2cb152c94bdba1533aab5ec4f25c77498
victorsaln <viktorsalnichenko@gmail.com>
2020-10-22 15:02:38 +0300

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Форма</title>
+</head>
+<body>
+<form action="/" method="GET">
+    <label for="firstname">Имя
+        <input type="text" name="firstname" placeholder="Введите имя">
+    </label><br>
+    <input type="text" name="secondname" placeholder="Введите фамилию"><br>
+    <input type="text" name="city" placeholder="Укажите город"><br>
+    <button type="submit">Отправить</button>
+</form>
+</body>
+</html>
